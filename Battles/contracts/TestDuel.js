af91d80815c894323a4620c840aa922d13c82f1f
johhonn <35551051+johhonn@users.noreply.github.com>
2019-08-30 12:00:38 -0400

    Update TestDuel.js
    Added move bytes decoder function

--- Battles/contracts/TestDuel.js
+++ Battles/contracts/TestDuel.js
@@ -130,6 +130,11 @@ function generateMoveSet(move1,move2,move3,move4,move5){
     return '0x0'+move1+'0'+move2+'0'+move3+'0'+move4+'0'+move5+'000000000000000000000000000000000000000000000000000000'
 }
 
+function decodeMoveSet(Movebytes){
+  let slice=Movebytes.slice(3,12)
+  let moves=[Number(slice[0]),Number(slice[2]),Number(slice[4]),Number(slice[6]),Number(slice[8])]
+  return moves
+}
 
 async function SimulateDuel(moves1,moves2,power1,power2,affinity1,affinity2){
     try{
