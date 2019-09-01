c3647f580da9aefe187d33aa600ab01cd62c4a59
Drew Taylor <drew@infiniteinternet.ca>
2019-09-01 12:12:21 -0400

    add firebase requirements

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBzUQmzf_kHpO_lU7FneB1xinVJifHfiUM",
+    authDomain: "cheeze-of-insight.firebaseapp.com",
+    databaseURL: "https://cheeze-of-insight.firebaseio.com",
+    projectId: "cheeze-of-insight",
+    storageBucket: "",
+    messagingSenderId: "84005637289",
+    appId: "1:84005637289:web:9252aa55eb5cf70b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
