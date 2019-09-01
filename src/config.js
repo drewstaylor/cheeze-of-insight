19fc89bd2d2bf94fd75a7622bb806c4a68503c6a
Drew Taylor <drew@infiniteinternet.ca>
2019-09-01 19:01:17 -0400

    fix typo in wizard image paths

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ config.apiMainnetBaseUrl = 'cheezewizards-mainnet.alchemyapi.io';
 config.apiRinkebyBaseUrl = 'cheezewizards-rinkeby.alchemyapi.io';
 config.mainnetTournamentContract = '0xec2203e38116f09e21bc27443e063b623b01345a';
 config.mainnetWizardsContract = '0x023C74B67dfCf4c20875A079e59873D8bBE42449';
-config.imageStorageUrl = 'https://storage.googleapis.com/cheeze-wizards-production/' + config.tTournamentContract + '/';
+config.imageStorageUrl = 'https://storage.googleapis.com/cheeze-wizards-production/' + config.mainnetTournamentContract + '/';
 config.proxyImageStorageUrl = 'https://cheezeofinsight.infiniteinternet.ca/svg/';
 config.openSeaTraits = 'api.opensea.io/collection/cheezewizard/';
 
