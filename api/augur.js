f4cef0d9ec2df13da4aef018a2416af883157b74
Drew Taylor <drew@infiniteinternet.ca>
2019-09-11 19:34:56 -0400

    augur tests passing

---
+++ api/augur.js
@@ -0,0 +1,67 @@
+const Augur = require('augur.js');
+let augur = new Augur();
+let coiOwnedMarkets = [];
+let communityMarkets = [];
+let allCoiMarkets = [];
+
+/**
+ * Get detailed market data for a market address
+ * @param {Object} markets: An `Array` of market addresses to resolve data for
+ */
+const getMarketDataByAddresses = async function (markets) {
+    if (!markets) {
+        return;
+    } else if (!markets.length) {
+        return;
+    }
+
+    augur.markets.getMarketsInfo({
+        marketIds: markets
+    }, (error, marketData) => {
+        console.log(marketData);
+        coiOwnedMarkets = marketData;
+    });
+}
+
+/**
+ * Get markets created by COI team
+ */
+const getCoiOwnedMarkets = function () {
+    augur.markets.getMarkets({
+        universe: "0x02149d40d255fceac54a3ee3899807b0539bad60",
+        creator: "0x88b49ca334521bada40faa71ef3b416fb1b161ec"
+    }, (error, markets) => {
+        console.log(markets);
+        getMarketDataByAddresses(markets);
+    });
+};
+
+const augurConnect = async function () {
+    // Connect to Augur node
+    augur.connect({
+        ethereumNode: {
+            httpAddresses: [
+                "https://rinkeby.infura.io" // hosted HTTP address for Ethereum Rinkeby test network
+            ],
+            wsAddresses: [
+                "wss://rinkeby.infura.io/ws" // hosted WebSocket address Ethereum Rinkeby test network
+            ]
+        },
+        augurNode: "wss://dev.augur.net/augur-node" // WebSocket address for an Augur Node on Rinkeby
+    }, (error, connectionInfo) => {
+        //console.log(connectionInfo);
+        console.log('Augur connected');
+        // Get COI Markets
+        getCoiOwnedMarkets();
+    });
+}
+
+async function construct() {
+    augurConnect();
+}
+
+construct();
+
+/*module.exports = {
+    augur: augur
+};*/
